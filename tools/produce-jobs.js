671b8661857ad130fc01014cd770018f41529db3
jayray <jasonray@jayray.net>
2016-05-10 15:51:41 -0400

    add support for number of messages

--- tools/produce-jobs.js
+++ tools/produce-jobs.js
@@ -1,5 +1,8 @@
-var FrequencyProducer = function(connection, frequency, content) {
+var FrequencyProducer = function(connection, frequency, max, content) {
     var self = this;
+
+    var count = 0;
+    var intervalHandle;
 
     var fivebeans = require('fivebeans');
     self.client = new fivebeans.client(connection.host, connection.port);
@@ -14,10 +17,13 @@ var FrequencyProducer = function(connection, frequency, content) {
 
     self.client.on('close', function(err) {
         console.log('client closed connection');
+        if (intervalHandle) {
+            clearInterval(intervalHandle);
+        }
     });
 
     function periodicallyQueueJobs() {
-        setInterval(queueJob, frequency);
+        intervalHandle = setInterval(queueJob, frequency);
     }
 
     const priority = 0;
@@ -28,7 +34,19 @@ var FrequencyProducer = function(connection, frequency, content) {
         var job = JSON.stringify(content);
         self.client.put(priority, delay, ttr, job, function(err, jobid) {
             console.log('queued job ' + jobid);
+            checkMax();
         });
+    }
+
+    function checkMax() {
+        if (max > 0) {
+            count = count + 1;
+            if (count >= max) {
+                console.log('reached max number of messages [%s], shutting down', max);
+                self.client.quit();
+            }
+        }
+
     }
 }
 
@@ -49,6 +67,7 @@ var argv = require('yargs')
     .default('port', '11300').alias('p', 'port')
     .default('tube', 'default').alias('t', 'tube')
     .default('frequency', 1000).alias('f', 'frequency')
+    .default('max', 0).alias('m', 'max')
     .argv;
 
 var connection = {
@@ -57,5 +76,5 @@ var connection = {
     tube: argv.tube
 };
 
-var producer = new FrequencyProducer(connection, argv.frequency, argv.payload);
+var producer = new FrequencyProducer(connection, argv.frequency, argv.max, argv.payload);
 producer.connect();

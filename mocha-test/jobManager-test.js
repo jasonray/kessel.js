6bc4592b1c5a96b6c06ee407bf419a6bed711f92
Jason Ray <jasonray@jayray.net>
2016-12-07 07:08:34 -0500

    working with job manager

---
+++ mocha-test/jobManager-test.js
@@ -0,0 +1,61 @@
+/*jslint node: true */
+"use strict";
+
+var mocha = require('mocha');
+var assert = require('assert');
+var JobManager = require('../lib/jobManager');
+
+describe('jobManager', function () {
+    it('init', function () {
+        var manager = new JobManager();
+    });
+    it('request job', function () {
+        var request = {
+            type: 'add',
+            payload: {
+                operands: [1, 2]
+            }
+        };
+        var manager = new JobManager();
+        manager.request(request);
+    });
+    it('process one job', function () {
+        var request = {
+            type: 'add',
+            payload: {
+                operands: [1, 2]
+            }
+        };
+        var manager = new JobManager();
+        manager.request(request);
+        manager.processOneJob();
+    });
+    it('when job is processed, if it contains a callback, callback fires', function (done) {
+        var myCallback = function () {
+            console.log('callback fired');
+            done();
+        }
+        var request = {
+            type: 'add',
+            callback: myCallback,
+            payload: {
+                operands: [1, 2]
+            }
+        };
+        var manager = new JobManager();
+        manager.request(request);
+        manager.processOneJob();
+    });
+    it('when job is processed, if it contains a callback but callback is not a function, callback does not fire', function () {
+        var request = {
+            type: 'add',
+            callback: 'hello',
+            payload: {
+                operands: [1, 2]
+            }
+        };
+        var manager = new JobManager();
+        manager.request(request);
+        manager.processOneJob();
+    });
+});

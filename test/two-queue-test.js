a0fb5e2594e288f2e546b05499435212161c734c
jason.ray <jasonray@jayray.net>
2016-12-10 16:45:14 -0500

    migrating tests to mocha

--- test/two-queue-test.js
+++ test/two-queue-test.js
@@ -1,13 +1,3 @@
 var Queue = require('../lib/queue');
 
-exports.dual = function(test) {
-    var queue1 = new Queue();
-    var queue2 = new Queue();
-    queue1.push('apple');
-    queue2.push('banana');
-    test.equals('apple', queue1.pop());
-    test.equals(null, queue1.pop());
-    test.equals('banana', queue2.pop());
-    test.equals(null, queue2.pop());
-    test.done();
-};
+

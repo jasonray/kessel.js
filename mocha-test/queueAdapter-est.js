d914e8b1d082cce9c49bc0f4b26f0a26eb2206e6
jason.ray <jasonray@jayray.net>
2016-12-10 15:26:52 -0500

    create basic tests for queue adapter

--- mocha-test/queueAdapter-est.js
+++ mocha-test/queueAdapter-est.js
@@ -6,11 +6,42 @@ var assert = require('assert');
 var QueueAdapter = require('../lib/QueueAdapter');
 
 describe('queueAdapter', function () {
-    it('initial size is 0', function () {
-        var queueAdapter = new QueueAdapter();
-        assert.equal(queueAdapter.size(), 0);
+    describe('size', function () {
+        it('initial size is 0', function () {
+            var queueAdapter = new QueueAdapter();
+            assert.equal(queueAdapter.size(), 0);
+        });
+        it('after enqueue, size is 1', function () {
+            var queueAdapter = new QueueAdapter();
+            var request = createSampleJobRequest();
+            queueAdapter.enqueue(request)
+            assert.equal(queueAdapter.size(), 1);
+        });
     });
-    // it('will fail', function () {
-    //     assert.ok(false);
-    // });
+    describe('isEmpty', function () {
+        it('initial isEmpty returns true', function () {
+            var queueAdapter = new QueueAdapter();
+            assert.equal(queueAdapter.isEmpty(), true);
+        });
+        it('after enqueue, isEmpty is false', function () {
+            var queueAdapter = new QueueAdapter();
+            var request = createSampleJobRequest();
+            queueAdapter.enqueue(request)
+            assert.equal(queueAdapter.isEmpty(), false);
+        });
+    });
 });
+
+function createSampleJobRequest(ref) {
+    var request = {
+        type: 'sample',
+        payload: {
+            x: 'x',
+            y: 'y'
+        }
+    };
+    if (ref) {
+        request.ref = ref;
+    }
+    return request;
+}

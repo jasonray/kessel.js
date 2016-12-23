8fb73da594aea4d0343624fc6a1df2dc080ac079
Jason Ray <jasonray@jayray.net>
2016-12-22 20:46:10 -0500

    move to event based processing

--- test/jobManager-test.js
+++ test/jobManager-test.js
@@ -5,6 +5,8 @@ var mocha = require('mocha');
 var assert = require('assert');
 var JobManager = require('../lib/jobManager');
 
+//TODO: disable logger on unit tests
+
 describe('jobManager', function () {
     it('init', function () {
         var manager = new JobManager();
@@ -19,43 +21,43 @@ describe('jobManager', function () {
         var manager = new JobManager();
         manager.request(request);
     });
-    it('process one job', function () {
-        var request = {
-            type: 'add',
-            payload: {
-                operands: [1, 2]
+
+    describe('process single job', function () {
+        it('process one job', function () {
+            var request = {
+                type: 'add',
+                payload: {
+                    operands: [1, 2]
+                }
+            };
+            var manager = new JobManager();
+            manager.processSingleJob(request);
+        });
+        it('when job is processed, if it contains a callback, callback fires', function (done) {
+            var myCallback = function () {
+                console.log('callback fired');
+                done();
             }
-        };
-        var manager = new JobManager();
-        manager.request(request);
-        manager._processOneJob();
-    });
-    it('when job is processed, if it contains a callback, callback fires', function (done) {
-        var myCallback = function () {
-            console.log('callback fired');
-            done();
-        }
-        var request = {
-            type: 'add',
-            callback: myCallback,
-            payload: {
-                operands: [1, 2]
-            }
-        };
-        var manager = new JobManager();
-        manager.request(request);
-        manager._processOneJob();
-    });
-    it('when job is processed, if it contains a callback but callback is not a function, callback does not fire', function () {
-        var request = {
-            type: 'add',
-            callback: 'hello',
-            payload: {
-                operands: [1, 2]
-            }
-        };
-        var manager = new JobManager();
-        manager.request(request);
-        manager._processOneJob();
+            var request = {
+                type: 'add',
+                callback: myCallback,
+                payload: {
+                    operands: [1, 2]
+                }
+            };
+            var manager = new JobManager();
+            manager.processSingleJob(request);
+        });
+        it('when job is processed, if it contains a callback but callback is not a function, callback does not fire', function () {
+            var request = {
+                type: 'add',
+                callback: 'hello',
+                payload: {
+                    operands: [1, 2]
+                }
+            };
+            var manager = new JobManager();
+            manager.processSingleJob(request);
+        });
     });
 });

c15698449009e9ed89e1018a42e47a1de6a59fe9
jason.ray <jasonray@jayray.net>
2016-12-28 16:46:17 -0500

    drop test

--- integration-test/forcefail-test.js
+++ integration-test/forcefail-test.js
@@ -8,8 +8,7 @@ var assert = require('assert');
  * Created by jason.ray on 12/28/16.
  */
 describe('force fail integration section', function () {
-    it('force fail test 1');
-    it('force fail test 2', function () {
+    it('force fail test', function () {
         assert.fail();
     });
 });

0d07c8b7281aa72d344b4d45ab88e23c6feae49a
jason.ray <jasonray@jayray.net>
2016-12-28 16:48:07 -0500

    disable the force fail integration test

--- integration-test/forcefail-test.js
+++ integration-test/forcefail-test.js
@@ -4,10 +4,7 @@
 var mocha = require('mocha');
 var assert = require('assert');
 
-/**
- * Created by jason.ray on 12/28/16.
- */
-describe('force fail integration section', function () {
+describe.skip('force fail integration section', function () {
     it('force fail test', function () {
         assert.fail();
     });

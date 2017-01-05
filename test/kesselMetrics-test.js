09436c65369d779baec8775a8c878220aadc855b
Jason Ray <jasonray@jayray.net>
2017-01-04 22:35:52 -0500

    reset metrics between tests

--- test/kesselMetrics-test.js
+++ test/kesselMetrics-test.js
@@ -10,8 +10,24 @@ var KesselMetrics = require('../lib/kesselMetrics');
 //TODO: disable logger on unit tests
 
 describe.only('metrics', function () {
+    beforeEach(function () {
+        var kesselMetrics = new KesselMetrics();
+        kesselMetrics.reset();
+    });
     it('using new constructor', function () {
-        var metrics = new KesselMetrics();
-        assert.ok(metrics);
+        var kesselMetrics = new KesselMetrics();
+        assert.ok(kesselMetrics);
+    });
+    it('increment and check', function () {
+        var kesselMetrics = new KesselMetrics();
+        kesselMetrics.metrics.gauges('jobs-published').value().should.equal(0);
+        kesselMetrics._jobPublished();
+        kesselMetrics.metrics.gauges('jobs-published').value().should.equal(1);
+    });
+    it('running twice works because of the reset, otherwise gauge would still be 1', function () {
+        var kesselMetrics = new KesselMetrics();
+        kesselMetrics.metrics.gauges('jobs-published').value().should.equal(0);
+        kesselMetrics._jobPublished();
+        kesselMetrics.metrics.gauges('jobs-published').value().should.equal(1);
     });
 });

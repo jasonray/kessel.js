0b5729e25a17cfb01ca9dfe8ad9aa5ec9fc099de
Jason Ray <jasonray@jayray.net>
2017-01-14 10:08:31 -0500

    es6 update

--- test/jobManager-test.js
+++ test/jobManager-test.js
@@ -1,28 +1,28 @@
 /*jslint node: true */
 "use strict";
 
-var mocha = require('mocha');
-var assert = require('assert');
-var should = require('should');
-var JobManager = require('../lib/jobManager');
+const mocha = require('mocha');
+const assert = require('assert');
+const should = require('should');
+const JobManager = require('../lib/jobManager');
 
 //TODO: disable logger on unit tests
 
 describe('jobManager', function () {
     describe('constructor', function () {
         it('using new constructor', function () {
-            var jobManager = new JobManager();
+            const jobManager = new JobManager();
             assert.ok(jobManager);
         });
     });
     it('request job', function (done) {
-        var request = {
+        const request = {
             type: 'add',
             payload: {
                 operands: [1, 2]
             }
         };
-        var manager = new JobManager();
+        const manager = new JobManager();
         manager.request(request, function (err) {
             assert.equal(err, null);
             done();
@@ -31,71 +31,71 @@ describe('jobManager', function () {
 
     describe('process single job', function () {
         it('process one job', function () {
-            var request = {
+            const request = {
                 type: 'add',
                 payload: {
                     operands: [1, 2]
                 }
             };
-            var manager = new JobManager();
-            var result = manager.processSingleJob(request);
+            const manager = new JobManager();
+            const result = manager.processSingleJob(request);
             assert.equal(result.value, 3);
         });
         it('when job is processed, if it contains a callback, callback fires', function (done) {
-            var myCallback = function (err, result) {
+            const myCallback = function (err, result) {
                 assert.equal(result.value, 3);
                 done();
             }
-            var request = {
+            const request = {
                 type: 'add',
                 callback: myCallback,
                 payload: {
                     operands: [1, 2]
                 }
             };
-            var manager = new JobManager();
-            var result = manager.processSingleJob(request);
+            const manager = new JobManager();
+            const result = manager.processSingleJob(request);
             assert.equal(result.value, 3);
         });
         it('when job is processed, if it contains a callback but callback is not a function, callback does not fire', function () {
-            var request = {
+            const request = {
                 type: 'add',
                 callback: 'invalid callback',
                 payload: {
                     operands: [1, 2]
                 }
             };
-            var manager = new JobManager();
+            const manager = new JobManager();
             manager.processSingleJob(request);
         });
         it('process one job delegates to correct handler', function () {
-            var request1 = {
+            const request1 = {
                 type: 'addition',
                 payload: {
                     operands: [3, 2]
                 }
             };
-            var request2 = {
+            const request2 = {
                 type: 'multiplication',
                 payload: {
                     operands: [3, 2]
                 }
             };
-            var manager = new JobManager();
+            const manager = new JobManager();
             assert.equal(manager.processSingleJob(request1).value, 5, "addition");
             assert.equal(manager.processSingleJob(request2).value, 6, "multiplication");
         });
     });
     describe('jobManager with async queue adapter', function () {
         it('process one job', function (done) {
-            var request = {
+            const request = {
                 type: 'add',
                 payload: {
                     operands: [3, 2]
                 },
                 callback: requestCallback
             };
-            var manager = new JobManager();
+            const manager = new JobManager();
             manager.initialize(function (err) {
                 manager.request(request, function (err) {
                     manager.start();

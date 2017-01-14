1f27abc0cc904f7d92253f54ae7d5e78cca44f7a
Jason Ray <jasonray@jayray.net>
2017-01-14 10:07:53 -0500

    es6 update

--- test/forcefail-test.js
+++ test/forcefail-test.js
@@ -1,8 +1,8 @@
 /*jslint node: true */
 "use strict";
 
-var mocha = require('mocha');
-var assert = require('assert');
+const mocha = require('mocha');
+const assert = require('assert');
 
 describe.skip('force fail unit test section', function () {
     it('force fail test', function () {

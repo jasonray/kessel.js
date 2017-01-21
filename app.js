5b7c2d43ae1b49c588d46d952603a14df77dc342
Jason Ray <jasonray@jayray.net>
2017-01-21 10:17:07 -0500

    make the queue strategy configurable

--- app.js
+++ app.js
@@ -10,8 +10,13 @@ context.config = require('./lib/config');
 logger.trace('standardConfig:', context.config.getConfig());
 logger.trace('config.beanstalk', context.config.getConfig('beanstalk'));
 
-var BeanstalkQueueAdapter = require('./lib/queue/beanstalkAdapter');
-context.queue = new BeanstalkQueueAdapter(context.config.getConfig('beanstalk'));
+logger.trace('queueStrategy:', context.config.getConfig('jobManager').queueStrategy);
+if (context.config.getConfig('jobManager').queueStrategy === 'beanstalk') {
+    var BeanstalkQueueAdapter = require('./lib/queue/beanstalkAdapter');
+    context.queue = new BeanstalkQueueAdapter(context.config.getConfig('beanstalk'));
+} else {
+    context.queue = null; //use default
+}
 
 logger.info('starting kessel app script');
 var manager = new JobManager(context);

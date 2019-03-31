bbfad288d5cef5f572c536e20b45bbbd96512a46
Jason Ray <jasonray@jayray.net>
2019-03-30 20:46:17 -0400

    code cleanup: const/var/let

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ context.config = require('./lib/config');
 logger.trace('standardConfig:', context.config.getConfig());
 logger.trace('config.beanstalk', context.config.getConfig('beanstalk'));
 
-var BeanstalkQueueAdapter = require('./lib/queue/beanstalkAdapter');
+const BeanstalkQueueAdapter = require('./lib/queue/beanstalkAdapter');
 context.queue = new BeanstalkQueueAdapter(context.config.getConfig('beanstalk'));
 
 logger.info('starting kessel app script');
-var manager = new JobManager(context);
+const manager = new JobManager(context);
 logger.trace('starting job manager');
 manager.start();
 
